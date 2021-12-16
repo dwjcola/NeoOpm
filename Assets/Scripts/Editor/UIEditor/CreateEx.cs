867fccbed854e93573ff33d15a7797c801dee602
dongwenjun <dongwenjun@oasgames.com>
2021-12-16 18:15:58 +0800

    添加不规则图形点击

--- Assets/Scripts/Editor/UIEditor/CreateEx.cs
+++ Assets/Scripts/Editor/UIEditor/CreateEx.cs
@@ -20,6 +20,7 @@ public class CreateEx : Editor
             }
         }
     }
+
     
     [MenuItem("GameObject/UI/Text - TextMeshPro")]
     static void CreatText()
---
+++ Assets/Scripts/UI/UIComponets/PolygonImage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ProHA
+{
+    [RequireComponent(typeof(PolygonCollider2D))]
+    public class PolygonImage : Image
+    {
+        private PolygonCollider2D _pc;
+
+        public PolygonCollider2D Pc
+        {
+            get
+            {
+                if (_pc == null)
+                {
+                    _pc = GetComponent<PolygonCollider2D>();
+                }
+
+                return _pc;
+            }
+        }
+
+        public override bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
+        {
+            Vector3 point;
+            RectTransformUtility.ScreenPointToWorldPointInRectangle(rectTransform, screenPoint, eventCamera, out point);
+            return Pc.OverlapPoint(point);
+        }
+    }
+}

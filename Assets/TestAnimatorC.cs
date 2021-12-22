e78b0ad76e8df41d653a35596b2170eb17105805
OASOFFICE\liuyubao <15192609657@163.com>
2021-12-22 20:38:18 +0800

    fix/battle 017

---
+++ Assets/TestAnimatorC.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnimatorC : MonoBehaviour
+{
+    public Animator animator;
+    float normalizedTransitionDuration = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogError(transform.localRotation);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            animator.CrossFade("Run", normalizedTransitionDuration);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.CrossFade("Hard_Attk_1", normalizedTransitionDuration);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.CrossFade("Light_Attk_1", normalizedTransitionDuration);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            animator.CrossFade("Dead_1", normalizedTransitionDuration);
+        }
+    }
+}

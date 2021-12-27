eea65e7eecf53a1ddcf46d898fba5647d8b38d72
dongwenjun <dongwenjun@oasgames.com>
2021-12-27 19:40:03 +0800

    增加自动和手动import setting

--- Assets/Scripts/Editor/UIEditor/PackTextureEditor.cs
+++ Assets/Scripts/Editor/UIEditor/PackTextureEditor.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEditor.U2D;
 using UnityEngine;
 using UnityEngine.U2D;
@@ -9,7 +11,7 @@ public class PackTextureEditor : AssetPostprocessor
 {
     private static string _atlasPath = "Assets/Resource_MS/UI/UIAtlas";
     private static string _texturePath = "Assets/Resource_MS/UISprites";
-
+    private static string _uiPath = "Assets/Resource_MS/UI";
     
     static void PackAtlasContents(string texturePath,string atlasName)
     {
@@ -84,7 +86,7 @@ public class PackTextureEditor : AssetPostprocessor
 
         SetAtlasFile();
     }
-    public void OnPreprocessTexture()
+    /*public void OnPreprocessTexture()
     {
         TextureImporter textureImporter = (TextureImporter)assetImporter;
         textureImporter.textureType = TextureImporterType.Sprite;
@@ -107,7 +109,101 @@ public class PackTextureEditor : AssetPostprocessor
             AssetDatabase.ImportAsset(path);
         }
         EditorUtility.ClearProgressBar();
-    }
+    }*/
+
+    #region Import Setting
+    /// <summary>
+    /// Android：不带透明通道压缩成ETC1，带透明通道压缩成ETC2，不被4整除fall back到RGBA32
+    ///IOS: 不带透明通道压缩成RGB PVRTC，带透明通道压缩成RGBA PVRTC ，不是2的整数次幂fall back到RGBA32
+    ///Texture图片拖入的时候，默认设置ToNerest，这样会自动保证Android平台下图片被4整除，IOS平台下图片是2的整除次幂。
+    /// </summary>
+    void OnPreprocessTexture()
+        {
+            TextureImporter importer = assetImporter as TextureImporter;
+            if (importer != null)
+            {
+                if (IsFirstImport(importer))
+                {
+                    if (assetPath.Contains(_uiPath))
+                    {
+                        importer.textureType = TextureImporterType.Sprite;
+                    }
+                    else
+                    {
+                        importer.textureType = TextureImporterType.Default;
+                    }
+
+                    importer.mipmapEnabled = false;
+                    importer.isReadable = false;
+                    TextureImporterPlatformSettings settings = importer.GetPlatformTextureSettings("iPhone");
+                    bool isPowerOfTwo = IsPowerOfTwo(importer);
+                    TextureImporterFormat defaultAlpha = isPowerOfTwo ? TextureImporterFormat.PVRTC_RGBA4 : TextureImporterFormat.ASTC_4x4;
+                    TextureImporterFormat defaultNotAlpha = isPowerOfTwo ? TextureImporterFormat.PVRTC_RGB4 : TextureImporterFormat.ASTC_6x6;
+                    settings.overridden = true;
+                    settings.format = importer.DoesSourceTextureHaveAlpha() ? defaultAlpha : defaultNotAlpha;
+                    importer.SetPlatformTextureSettings(settings);
+     
+                    settings = importer.GetPlatformTextureSettings("Android");
+                    settings.overridden = true;
+                    settings.allowsAlphaSplitting = false;
+                    bool divisible4 = IsDivisibleOf4(importer);
+                    defaultAlpha = divisible4 ? TextureImporterFormat.ETC2_RGBA8Crunched : TextureImporterFormat.ASTC_4x4;
+                    defaultNotAlpha = divisible4 ? TextureImporterFormat.ETC_RGB4Crunched : TextureImporterFormat.ASTC_6x6;
+                    settings.format = importer.DoesSourceTextureHaveAlpha() ? defaultAlpha : defaultNotAlpha;
+                    importer.SetPlatformTextureSettings(settings);
+                }
+            }
+        }
+        //被4整除
+        bool IsDivisibleOf4(TextureImporter importer)
+        {
+            (int width, int height) = GetTextureImporterSize(importer);
+            return (width % 4 == 0 && height % 4 == 0);
+        }
+     
+        //2的整数次幂
+        bool IsPowerOfTwo(TextureImporter importer)
+        {
+            (int width, int height) = GetTextureImporterSize(importer);
+            return (width == height) && (width > 0) && ((width & (width - 1)) == 0);
+        }
+     
+        //贴图不存在、meta文件不存在、图片尺寸发生修改需要重新导入
+        bool IsFirstImport(TextureImporter importer)
+        {
+            (int width, int height) = GetTextureImporterSize(importer);
+            Texture tex = AssetDatabase.LoadAssetAtPath<Texture2D>(assetPath);
+            bool hasMeta = File.Exists(AssetDatabase.GetAssetPathFromTextMetaFilePath(assetPath));
+            return tex == null || !hasMeta || (tex.width != width && tex.height != height);
+        }
+     
+        //获取导入图片的宽高
+        (int, int) GetTextureImporterSize(TextureImporter importer)
+        {
+            if (importer != null)
+            {
+                object[] args = new object[2];
+                MethodInfo mi = typeof(TextureImporter).GetMethod("GetWidthAndHeight", BindingFlags.NonPublic | BindingFlags.Instance);
+                mi.Invoke(importer, args);
+                return ((int)args[0], (int)args[1]);
+            }
+            return (0, 0);
+        }
+        static bool forceImport = false;
+        //放入UI目录得图强制更新
+        static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            foreach (string str in movedAssets)
+            {
+                if (str.Contains("UI保存路径"))
+                {
+                    forceImport = true;             //重新reimport并且强制 IsFirstImport
+                    AssetDatabase.ImportAsset(str); // 如果是移动到指定UI目录下，需要重新Import
+                }
+            }
+        }
+
+    #endregion
     //生成图集配置文件
     [MenuItem("Tools/SetAtlasFile")]
     static void SetAtlasFile()
@@ -142,7 +238,5 @@ public class PackTextureEditor : AssetPostprocessor
             File.WriteAllLines(outfile,lines.ToArray());
             AssetDatabase.Refresh();
         }
-        
-        
     }
 }
---
+++ Assets/Scripts/Editor/UIEditor/TextureSetting.cs
@@ -0,0 +1,266 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+using Object = UnityEngine.Object;
+
+/// <summary>
+/// 批量图片资源导入设置
+/// 使用说明： 选择需要批量设置的文件夹或贴图，
+/// 单击Tools/Texture Import Settings，
+/// 打开窗口后选择对应参数，
+/// 点击Set Texture ImportSettings，
+/// 稍等片刻，--批量设置成功。
+/// 
+/// by dwj
+/// </summary>
+
+
+public class TextureSetting : EditorWindow
+{
+
+    /// <summary>
+    /// 临时存储int[]
+    /// </summary>
+    private int[] IntArray = new int[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+    //AnisoLevel
+    [SerializeField]
+    private static int AnisoLevel = 1;
+
+    //Filter Mode
+    private static FilterMode mFilterMode = FilterMode.Point;
+    //Wrap Mode
+    private static TextureWrapMode mTextureWrapMode = TextureWrapMode.Clamp;
+    //Texture Type
+    private static TextureImporterType TextureType = TextureImporterType.Default;
+    //Max Size
+    private static int MaxSizeInt = 5;
+    private string[] MaxSizeString = new string[] { "32", "64", "128", "256", "512", "1024", "2048", "4096" };
+    //Format
+    private static TextureImporterFormat ImporterFormat = TextureImporterFormat.AutomaticCompressed;
+
+    private static TextureImporterMipFilter mTextureImporterMipFilter = TextureImporterMipFilter.BoxFilter;
+
+    private static bool ReadAndWrite = false;
+
+    private static bool grayscaleToAlpha = false;
+    private static bool alphaIsTransparency = false;
+
+    private static bool Mipmaps = false;
+
+    private static bool InLinearSpace = false;
+
+    private static bool BorderMapMaps = false;
+
+    private static bool FadeoutMapMaps = false;
+
+    private static bool OverrideOnAndroid = false;
+    private static bool OverrideOnIOS = false;
+    private static int IOSMaxSizeInt = 5;
+    private static int AndroidMaxSizeInt = 5;
+
+    private static TextureImporterFormat IOSImporterFormat = TextureImporterFormat.AutomaticCompressed;
+    private static TextureImporterFormat AndroidImporterFormat = TextureImporterFormat.AutomaticCompressed;
+
+    /// <summary>
+    /// 创建、显示窗体
+    /// </summary>
+    [MenuItem("Tools/Texture Import Settings")]
+    private static void Init()
+    {
+        TextureSetting window = (TextureSetting)EditorWindow.GetWindow(typeof(TextureSetting), true, "TextureImportSetting");
+        window.Show();
+    }
+
+    //string Cur2DtexturePath = "";
+    /// <summary>
+    /// 显示窗体里面的内容
+    /// </summary>
+    private void OnGUI()
+    {
+        GUILayout.Label("please select folder or Texture");
+
+
+        ReadAndWrite = GUILayout.Toggle(ReadAndWrite, "Read/Write");
+
+        //Texture Type
+        TextureType = (TextureImporterType)EditorGUILayout.EnumPopup("Texture Type", TextureType);
+        grayscaleToAlpha = GUILayout.Toggle(grayscaleToAlpha, "Alpha from Grayscale");
+        alphaIsTransparency = GUILayout.Toggle(alphaIsTransparency, "Alpha Is Transparency");
+
+        //Wrap Mode
+        mTextureWrapMode = (TextureWrapMode)EditorGUILayout.EnumPopup("Wrap Mode", mTextureWrapMode);
+
+        Mipmaps = GUILayout.Toggle(Mipmaps, "Mipmaps");
+        if (Mipmaps)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(20f);
+            InLinearSpace = GUILayout.Toggle(InLinearSpace, "In Linear Space");
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(20f);
+            BorderMapMaps = GUILayout.Toggle(BorderMapMaps, "Border Map Maps");
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(20f);
+            //Filter Mode
+            mTextureImporterMipFilter = (TextureImporterMipFilter)EditorGUILayout.EnumPopup("Mip Map Filtering", mTextureImporterMipFilter);
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(20f);
+            FadeoutMapMaps = GUILayout.Toggle(FadeoutMapMaps, "Fadeout Map Maps");
+            GUILayout.EndHorizontal();
+        }
+
+        //Filter Mode
+        mFilterMode = (FilterMode)EditorGUILayout.EnumPopup("Filter Mode", mFilterMode);
+
+        //Max Size
+        MaxSizeInt = EditorGUILayout.IntPopup("Max Size", MaxSizeInt, MaxSizeString, IntArray);
+        //Format
+        ImporterFormat = (TextureImporterFormat)EditorGUILayout.EnumPopup("Format", ImporterFormat);
+        //AnisoLevel
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Aniso Level  ");
+        AnisoLevel = EditorGUILayout.IntSlider(AnisoLevel, 0, 9);
+        GUILayout.EndHorizontal();
+
+        OverrideOnAndroid = GUILayout.Toggle(OverrideOnAndroid, "Override for Android");
+        if (OverrideOnAndroid)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(20f);
+            //Max Size
+            AndroidMaxSizeInt = EditorGUILayout.IntPopup("Max Size", AndroidMaxSizeInt, MaxSizeString, IntArray);
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(20f);
+            AndroidImporterFormat = (TextureImporterFormat)EditorGUILayout.EnumPopup("Format", AndroidImporterFormat);
+            GUILayout.EndHorizontal();
+
+        }
+        OverrideOnIOS = GUILayout.Toggle(OverrideOnIOS, "Override for IOS");
+        if (OverrideOnIOS)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(20f);
+            //Max Size
+            IOSMaxSizeInt = EditorGUILayout.IntPopup("Max Size", IOSMaxSizeInt, MaxSizeString, IntArray);
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(20f);
+            //Format
+            IOSImporterFormat = (TextureImporterFormat)EditorGUILayout.EnumPopup("Format", IOSImporterFormat);
+            GUILayout.EndHorizontal();
+        }
+
+        if (GUILayout.Button("Set Texture ImportSettings"))
+            LoopSetTexture();
+    }
+
+    //string SelectPath;
+
+    /// <summary>
+    /// 获取贴图设置
+    /// </summary>
+    public TextureImporter GetTextureSettings(string path)
+    {
+        TextureImporter textureImporter = AssetImporter.GetAtPath(path) as TextureImporter;
+
+        textureImporter.isReadable = ReadAndWrite;
+
+        textureImporter.mipmapEnabled = Mipmaps;
+        textureImporter.alphaIsTransparency = alphaIsTransparency;
+        textureImporter.grayscaleToAlpha = grayscaleToAlpha;
+
+        if (textureImporter.mipmapEnabled)
+        {
+            textureImporter.borderMipmap = BorderMapMaps;
+            textureImporter.fadeout = FadeoutMapMaps;
+            textureImporter.generateMipsInLinearSpace = InLinearSpace;
+            textureImporter.mipmapFilter = mTextureImporterMipFilter;
+        }
+        //AnisoLevel
+        textureImporter.anisoLevel = AnisoLevel;
+        //Filter Mode
+        textureImporter.filterMode = mFilterMode;
+
+        //Wrap Mode
+        textureImporter.wrapMode = mTextureWrapMode;
+
+        //Texture Type
+        textureImporter.textureType = TextureType;
+        //Max Size 
+        textureImporter.maxTextureSize = Convert.ToInt32(MaxSizeString[MaxSizeInt]);
+        //Format
+        textureImporter.textureFormat = ImporterFormat;
+
+
+        if (OverrideOnAndroid)
+        {
+            int AndroidMaxSize = Convert.ToInt32(MaxSizeString[AndroidMaxSizeInt]);
+
+            textureImporter.SetPlatformTextureSettings("Android", AndroidMaxSize, AndroidImporterFormat);
+        }
+        else
+        {
+            textureImporter.ClearPlatformTextureSettings("Android");
+        }
+
+        if (OverrideOnIOS)
+        {
+            textureImporter.SetPlatformTextureSettings("iPhone", Convert.ToInt32(MaxSizeString[IOSMaxSizeInt]), IOSImporterFormat);
+        }
+        else
+        {
+            textureImporter.ClearPlatformTextureSettings("iPhone");
+        }
+
+        return textureImporter;
+    }
+
+
+    /// <summary>
+    /// 循环设置选择的贴图
+    /// </summary>
+    private void LoopSetTexture()
+    {
+        Object objSelected = Selection.activeObject;
+        if (objSelected != null)
+        {
+            Object[] textures = GetSelectedTextures();
+            Selection.objects = new Object[0];
+            foreach (Texture2D texture in textures)
+            {
+                string path = AssetDatabase.GetAssetPath(texture);
+                SetTexture(path);
+
+            }
+
+        }
+    }
+
+
+    private void SetTexture(string path)
+    {
+        TextureImporter texImporter = GetTextureSettings(path);
+        TextureImporterSettings tis = new TextureImporterSettings();
+        texImporter.ReadTextureSettings(tis);
+        texImporter.SetTextureSettings(tis);
+        AssetDatabase.ImportAsset(path);
+    }
+    /// <summary>
+    /// 获取选择的贴图
+    /// </summary>
+    /// <returns></returns>
+    private Object[] GetSelectedTextures()
+    {
+        return Selection.GetFiltered(typeof(Texture2D), SelectionMode.DeepAssets);
+    }
+}
